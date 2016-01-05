71f2a8f69a94511920755767cfd2b7462ea16a28
nithin <nithinjosetom@gmail.com>
2016-01-06 00:51:51 +0530

    Added insert function

--- insert.c
+++ insert.c
@@ -1,15 +1,15 @@
 void insert(struct node **beg, int val){
 	if(*beg == NULL){
 		struct node *temp= (struct node*)malloc(sizeof(struct node));
-		(temp)->val=val;
-		(temp)->next=NULL;
+		(temp)->num=val;
+		(temp)->link=NULL;
 		(*beg)=temp;
 	}
 	else{
 		//printf("\n Entered else");
 		struct node *temp= (struct node*)malloc(sizeof(struct node));
-		temp->val=val;
-		temp->next=*beg;
+		temp->num=val;
+		temp->link=*beg;
 		(*beg)=temp;
 						
 	}
